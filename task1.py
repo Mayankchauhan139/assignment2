9afaf9ba30dd4539533cd38fe6406a950101d28f
mayankchauhan139 <mayank.chauhan130904@gmail.com>
2025-09-04 19:04:34 +0530

    adding the code

---
+++ task1.py
@@ -0,0 +1,10 @@
+#enter a number and convert the input to integer
+num = int(input("Enter a number: "))
+
+# Checking if the number is even
+if num % 2 == 0:
+    # Print if the number is even
+    print(num,"is an even number.")
+else:
+    # Print if the number is odd
+    print(num,"is an odd number.")
---
+++ task2.py
@@ -0,0 +1,4 @@
+num=0
+for i in range(51):
+    num=num + i
+print(f"The sum of numbers from 1 to 50 is: {num}")
